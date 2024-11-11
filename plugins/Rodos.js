e383e174dbb62e51b76b97ab0e7baa5b9a00d374
Abdulkareem albukari <113518431+Aurtherle@users.noreply.github.com>
2024-11-12 00:45:24 +0300

    Create Rodos.js

---
+++ plugins/Rodos.js
@@ -0,0 +1,126 @@
+ let handler = m => m 
+ handler.all = async function (m) { 
+ let chat = global.db.data.chats[m.chat] 
+
+ if (/^آرثر$/i.test(m.text) ) { //sem prefixo 
+ conn.reply(m.chat, `*قلب آرثر*`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+
+ } 
+
+
+
+
+if (/^سم$/i.test(m.text) ) { //sem prefixo 
+ conn.reply(m.chat, `*سم الله عدوك*`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+
+}
+
+
+
+if (/^كلزق$/i.test(m.text) ) { //sem prefixo 
+ conn.reply(m.chat, `*ما اتخيلك ف صحن*`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+
+}
+
+
+if (/^كل زق$/i.test(m.text) ) { //sem prefixo 
+ conn.reply(m.chat, `*ما اتخيلك ف صحن*`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+
+}
+
+
+if (/^كل تبن$/i.test(m.text) ) { //sem prefixo 
+ conn.reply(m.chat, `*حط نفسك ف صحن وتعال*`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+
+}
+
+if (/^دزمها$/i.test(m.text) ) { //sem prefixo 
+ conn.reply(m.chat, `*استح*`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+
+}
+
+ if (/^تصت$/i.test(m.text) ) { //sem prefixo 
+ conn.reply(m.chat, `*تست*`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+
+ } 
+
+
+ if (/^احبك$/i.test(m.text) ) { //sem prefixo 
+ conn.reply(m.chat, `*اقول بلا قرف*`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+
+ } 
+
+
+   if (/^اما يا ارثر$/i.test(m.text) ) { //sem prefixo 
+    conn.reply(m.chat, `*مب ناقصني*`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+
+    } 
+
+
+ if (/^عمك ارثر$/i.test(m.text) ) { //sem prefixo 
+ conn.reply(m.chat, `*صدقت*`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+
+ } 
+
+
+ if (/^دينجي$/i.test(m.text) ) { //sem prefixo 
+ conn.reply(m.chat, ` *ماكيما عمته*, `, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+
+ } 
+
+
+
+ if (/^بوت زق$/i.test(m.text) ) { //sem prefixo 
+ conn.reply(m.chat, `*امااا يطاوعك قلبك علي ؟*`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+
+ } 
+
+
+ if (/^ملل$/i.test(m.text) ) { //sem prefixo 
+ conn.reply(m.chat, `*طيب اسكت*`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+
+ } 
+
+ if (/^السلام عليكم$/i.test(m.text) ) { //sem prefixo 
+ conn.reply(m.chat, `*وعليكم السلام*`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+
+}
+
+ if (/^لُجين$/i.test(m.text) ) { //sem prefixo 
+ conn.reply(m.chat, `*الممرضة الرقله*`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+
+ } 
+
+ if (/^تصبح على خير/i.test(m.text) ) { //sem prefixo 
+ conn.reply(m.chat, `*تلاقي الخير*`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+ }
+
+
+
+ if (/^لوجين$/i.test(m.text) ) { //sem prefixo 
+ conn.reply(m.chat, `*خلك منها فاشلة*`, m) //wm, null, [['Menu', '#menu']], m) botones :V
+
+}
+
+
+ if (/^ميسي$/i.test(m.text) ) { //sem prefixo 
+ conn.reply(m.chat, `*القوت العم*`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+
+}
+
+ if (/^ارثر من عمك$/i.test(m.text) ) { //sem prefixo 
+ conn.reply(m.chat, `*ارثر*`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+
+}
+
+ if (/^برب$/i.test(m.text) ) { //sem prefixo 
+ conn.reply(m.chat, `*تروح وترجع ب السلامة ننتظرك*`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+
+}
+
+
+   if (/^بوت$/i.test(m.text) ) { //sem prefixo 
+ conn.reply(m.chat, `*لبيه ؟*`, m) //wm, null, [['Menu', '#menu']], m) botones :V
+   }
+ return !0 } 
+ export default handler
