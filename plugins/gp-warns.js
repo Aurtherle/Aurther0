c3e0165342e48b4259d934801fed9d4e72922a62
Abdulkareem albukari <113518431+Aurtherle@users.noreply.github.com>
2024-11-11 20:11:20 +0300

    Update gp-warns.js

--- plugins/gp-warns.js
+++ plugins/gp-warns.js
@@ -1,24 +1,21 @@
-let handler = async (m, { conn, args, groupMetadata }) => {
-  let who = m.quoted
-    ? m.quoted.sender
-    : m.mentionedJid && m.mentionedJid[0]
-      ? m.mentionedJid[0]
-      : m.fromMe
-        ? conn.user.jid
-        : m.sender
-  if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
-  let warn = global.db.data.users[who].warn
-  let name = conn.getName(who)
-  m.reply(`
- *WARNINGS*
 
-▢ *Name :* ${name} 
-▢ *Warns :* ${warn}`)
+let handler = async (m, { conn, args, groupMetadata}) => {
+       let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+       if (!(who in global.db.data.users)) throw `المستخدم غير موجود ف قاعدة بياناتي`
+       let warn = global.db.data.users[who].warn
+       let name = conn.getName(who)
+      m.reply(`
+*❃ ──────⊰ ❀ ⊱────── ❃*
+          *عدد الإنذارات*
+*❃ ──────⊰ ❀ ⊱────── ❃*
+◍ *الأسم :* ${name} 
+◍ *الإنذارات :* ${warn}
+*❃ ──────⊰ ❀ ⊱────── ❃*`)
 }
 
 handler.help = ['warns']
 handler.tags = ['group']
-handler.command = ['warns']
+handler.command = ['انذارات'] 
 handler.group = true
 
 export default handler
