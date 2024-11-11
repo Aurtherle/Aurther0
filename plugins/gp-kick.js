bfa69eb89a07d8e0fb83443db19b0b60e005dada
Abdulkareem albukari <113518431+Aurtherle@users.noreply.github.com>
2024-11-11 06:42:12 +0300

    Update gp-kick.js

--- plugins/gp-kick.js
+++ plugins/gp-kick.js
@@ -1,17 +1,19 @@
-let handler = async (m, { conn, participants, usedPrefix, command }) => {
-  let kickte = `✳️ Correct use of the command\n*${usedPrefix + command}* @tag`
 
-  if (!m.mentionedJid[0] && !m.quoted)
-    return m.reply(kickte, m.chat, { mentions: conn.parseMention(kickte) })
-  let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
-  let owr = m.chat.split`-`[0]
-  await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
-  m.reply(`✅ User kicked`)
+let handler = async (m, { conn, participants, usedPrefix, command }) => {
+
+let kickte = `*مــنشـن الـشـخص !*`
+
+if (!m.mentionedJid[0] && !m.quoted) return m.reply(kickte, m.chat, { mentions: conn.parseMention(kickte)}) 
+let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
+let owr = m.chat.split`-`[0]
+await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
+m.reply(`*تـــم الــطرد !*`) 
+
 }
 
 handler.help = ['kick @user']
 handler.tags = ['group']
-handler.command = ['kick', 'expulsar']
+handler.command = ['اتوكل','كلبطه','اخرس','طرد','دزمها','احبك','اكرهك','مغربي','تصرف خاطئ','يمني'] 
 handler.admin = true
 handler.group = true
 handler.botAdmin = true
